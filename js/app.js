e1232a88f6c75166ac811718a82a6b0336747c59
BlindNerd <blindnerd@Blinds-MacBook-Pro.local>
2017-09-01 10:50:23 -0400

    Its done
    Awesome..

--- js/app.js
+++ js/app.js
@@ -1,135 +1,222 @@
+'use strict'
 
 const express = require('express');
+const app = express();
 const config = require('./config.js');
 const moment = require('moment');
 const Twit = require('twit');
 const bodyParser = require('body-parser');
-const app = express();
-const errFriends = new Error('Looks like we have an error in the friends function!');
-const errTweet = new Error('Looks like we have an error in the tweets function');
-const errMessages = new Error('Looks like we have an error in the messages function');
-const errRecieved = new Error('Looks like we have an error in the recieved function');
+const http = require('http');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
+
+  // Build up constructor to pass into app.use to save on twitter calls
+
+let dmessages = [ ];
+let message1 = [ ];
+let myInfo = [ ];
+let tweetMessage = [ ];
+let friendsInfo = [ ];
+
+// add the moment module to pug
+app.locals.moment = require('moment');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// add the styles to the page and make the cleint side java script a js file to fix the error
 app.use('/static', express.static('public'));
 const T = new Twit(config);
 
 app.set('view engine', 'pug');
 
-/*---------------------------------------------
-Create an error function to pass into all functions
-each function to have its own error message
--------------------------------------------------*/
-
-const error = (mes) => {
-  console.error(mes);
-  throw err;
-};
-
 /*----------------------------------------
 Get all info from twitter with twit and get it ready for express
-make sure create functions to pass into the express
+make sure create functions to pass into the express had to make them
+there own functions to give the app time to retrieve
 -------------------------------------------*/
 
-// Get profile information verify account
+function Me(name, screenName, image, friends, banner) {
+  this.name = name;
+  this.screenName = screenName;
+  this.image = image;
+  this.friendsCount = friends;
+  this.banner = banner;
+}
 
-const profile = (req, res, next) => {
+// Get profile information verify account
+// Build a constructor to grab the screenName
+
+
   T.get('account/verify_credentials', { skip_status: true })
     .catch((err) => {
       console.log('caught error at getting credintials', err.stack)
     })
     .then( (result) => {
-      req.info = result.data;
-      //console.log(info);
-    })// end of get profile then
-    setTimeout(next, 1000);
-}; // end of profile function
+      let name = result.data.name;
+      let screenName = result.data.screen_name;
+      let image = result.data.profile_image_url;
+      let friendsCount = result.data.friends_count;
+      let banner = result.data.profile_banner_url;
+myInfo = new Me(name, screenName, image, friendsCount, banner);
+ })// end of get profile then
+
 
 // get 5 most recent friends
+function Friendcon(name, user, image){
+  this.name = name;
+  this.screenName = user;
+  this.friendImage = image;
+}
 
-const friends = (req, res, next) => {
-  T.get('friends/list', { screen_name: req.info.screen_name, count: 9, include_user_entities: false }, (err, data, response) => {
+T.get('friends/list', { screen_name: Me.screenName, count: 9, include_user_entities: false }, (err, data, response) => {
     if (err) {
-      error(errFriends);
+       const err = new Error('You made to many calls to Twitter,  wait a couple of minutes and try agian.');
+       console.error(err);
+       err.status = 500;
+       throw err;
     }else {
-       req.friendsList = data.users;
-      //console.log(req.friendsList);
-    }
-  }) // end of get friends list
-   setTimeout(next, 2000);
-};// end of friends function
+    for (let i = 0; i < data.users.length; i++) {
+      let friendsName = data.users[i].name;
+      let friendsUser = data.users[i].screen_name;
+      let friendsImage = data.users[i].profile_image_url;
+      friendsInfo.push(new Friendcon(friendsName, friendsUser, friendsImage));
+     } // end of loop
+    }}); // end of get friends list
 
+// build up tweet constructor
 
-// get the 5 most recent tweets
+function TweetData(text, created, name, user, retweet, likes) {
+  this.message = text;
+  this.timeCreated = created;
+  this.name = name;
+  this.user = user;
+  this.retweet = retweet;
+  this.likes = likes;
+}
 
-const tweets = (req, res, next) => {
-  T.get('statuses/user_timeline', { screen_name: req.info.screen_name, count: 9 }, (err, data, response) => {
+// get the 5 most recent tweets when app loads
+
+T.get('statuses/user_timeline', { screen_name: Me.screenName, count: 9 }, (err, data, response) => {
     if (err) {
-      error(errTweet);
+      const err = new Error('Looks like we had a problem getting your tweets.');
+      console.error(err);
+      err.status = 500;
+      throw err;
     }else {
-      req.tweet = data;
-      //console.log(req.tweet);
-      }
-    });// end of get tweets
-    setTimeout(next, 1000);
-};// end of tweet function
+    for (let i = 0; i < data.length; i++) {
+       let tweetText = data[i].text;
+       let tweetTime = data[i].created_at;
+       let tweetCreated = moment(tweetTime, "ddd MMM HH:mm:ss Z YYYY").format("MM/DD/YYYY, HH:mm");
+       let tweetName = data[i].user.name;
+       let tweetUser = data[i].user.screen_name;
+       let tweetRetweet = data[i].retweet_count;
+       let tweetLikes = data[i].favorite_count;
+  tweetMessage.push(new TweetData(tweetText, tweetCreated, tweetName, tweetUser, tweetRetweet, tweetLikes));
+    }// end of loop
+  }});// end of get tweets and else
 
+// Build a constuctor function to handle the messages sent and recieved
+
+  // This is the recieved messages contructor
+function Rmessages(text, name, imageURL, timeCreated) {
+  this.text = text;
+  this.name = name;
+  this.image = imageURL;
+  this.time = timeCreated;
+}
+  // my sent messages contructor
+function Messages(text, time) {
+  this.text = text;
+  this.time = time;
+}
 
   // get 5 private messages sent
 
-const messages = (req, res, next) => {
-   T.get('direct_messages/sent', {screen_name: req.info.screen_name, count: 5}, (err, data, response) => {
+T.get('direct_messages/sent', {screen_name: Me.screenName, count: 5}, (err, data, response) => {
      if (err) {
-       error(errMessages);
+       const err = new Error('Looks like we had a problem getting your sent messages.');
+       console.error(err);
+       err.status = 500;
+       throw err;
      }else {
-       req.message = data;
-     console.log(data);
-     }
+     for (let i = 0; i < data.length; i++) {
+       let messageData = data[i].text;
+       let messageTime1 = data[i].created_at;
+       let messageTime = moment(messageTime1, "ddd MMM HH:mm:ss Z YYYY").format("MM/DD/YYYY, HH:mm");
+    message1.push(new Messages(messageData, messageTime));
+     }// end of loop
+    }// end of else
    }); // end of get messages
-   setTimeout(next, 1500);
-};// end of dirMessages
 
 // Get 5 messages recieved
 
-const recieved = (req, res, next) => {
-   T.get('direct_messages', {screen_name: req.info.screen_name, count: 5}, (err, data, response) => {
+T.get('direct_messages', {screen_name: Me.screenName}, (err, data, response) => {
      if (err) {
-       error(errRecieved);
+       const err = new Error('Looks like we had a problem getting your messages.');
+       console.error(err);
+       err.status = 500;
+       next(err);
      }else {
-       req.recMessage = data;
-       //console.log(data);
-   }
- }); // end of get messages
-   setTimeout(next, 1500);
-};// end of dirMessages
+    for (let i = 0; i < data.length; i++) {
+      let rmessage = data[i].text;
+      let sender = data[i].sender.name;
+      let senderImage = data[i].sender.profile_image_url;
+      let time1 = data[i].created_at;
+      let time = moment(time1, "ddd MMM HH:mm:ss Z YYYY").format("MM/DD/YYYY, HH:mm");
+      // build up each instance of recieved messages
+    dmessages.push(new Rmessages(rmessage, sender, senderImage, time));
+    }// end of loop
+  }// end of else
 
+ }); // end of get messages
 
 /*---------------------------------------------
-load all functions into express with the app.use
+pass contructors into app.get so that pug can use
+the data
 -----------------------------------------------*/
-app.use(profile, tweets, friends, messages, recieved);
 
- app.get('/', (req, res) => {
-   let timeStamp = moment(req.tweet.created_at).format('lll');
-   let messageStamp = moment(req.message.created_at).format('lll');
-   res.render('index', {
-     profile: req.info,
-     tweets: req.tweet,
-     friends: req.friendsList,
-     timeStamp: timeStamp,
-     messages: req.message,
-     messageStamp: messageStamp,
-     recievedMessage: req.recMessage
+app.get('/', (req, res) => {
+   res.render('index', {myInfo, friendsInfo, tweetMessage, message1, dmessages})
 
-   }); // end of render
+ // open a conection to send this with socket
 
- }) // end of app.get
+ io.on('connection', function (socket) {
+   socket.emit('info', myInfo);
+  });// end of connection
+}); // end of app get
+
+// recieve text from client to post to twitter
+io.on('connection', function (socket) {
+    socket.on('message', function (text) {
+        let tweetText1 = text;
+T.post('statuses/update', {status: tweetText1},(err, data, res) => {
+   });// end of T.post
+  }); // end of socket on
+}); // end of app.post
+
+// create a friendly page not found error to pass into the error handler
+ app.use((req, res, next) => {
+   const err = new Error('I\'m sorry that page does not exist.');
+   err.status = 404;
+   next(err);
+ });// end of 404 error page
+
+// make an error handler to pass errors through
+
+ app.use( (err, req, res, next) => {
+   res.locals.error = err;
+
+   // had an error caused by one of the modules this was a workaround
+  if ( res.status !== 404 ) {
+    res.status = 500;
+  }else {
+   res.status(err.status)
+ }
+   res.render('error');
+ });// end of error handler
 
 
-
-
-
-app.listen(3000, () => {
-  console.log('The application is running on localhost:3000.');
+server.listen(3000, () => {
+  console.log('This application is running on localhost:3000.');
 });
